7bc3baee10c516fa4df24989f99368aa785c13d1
Abdullah Alhabal <abdulllah.al.habbal@gmail.com>
2024-11-29 17:25:34 +0300

    chore: initial project setup with development tools nstalled and configured Laravel IDE Helper for better IDE support Added PHP CS Fixer for code style enforcement Configured Laravel Pint for automatic code formatting Integrated PHPStan for static code analysis Installed Swagger for API documentation generation Added Laravel Debugbar and Telescope for debugging and monitoring Configured Laravel Pulse for application health checks Added necessary annotations for Swagger and validated documentation generation Updated .gitignore and .gitattributes for Laravel-specific exclusions Ran 'php artisan l5-swagger:generate' to ensure proper Swagger setup

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Providers;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        if ($this->app->environment('local')) {
+            $this->app->register(\Laravel\Telescope\TelescopeServiceProvider::class);
+            $this->app->register(TelescopeServiceProvider::class);
+        }
+    }
+
+    public function boot(): void
+    {
+        Gate::define('viewPulse', function (User $user) {
+            return $user->isAdmin();
+        });
+    }
+}
